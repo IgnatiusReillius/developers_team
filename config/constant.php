d8394b02a518eb36f08176196c5bdcffc82fdbcd
mikelji97 <mikel.falcesino@gmail.com>
2025-10-28 04:09:58 +0100

    Implementar constantes utilizadas

--- config/constant.php
+++ config/constant.php
@@ -8,7 +8,7 @@ if (!defined('ROOT_PATH')) {
 if (!defined('VIEWS_PATH')) {
     define('VIEWS_PATH', __DIR__ . '/../app/views/');
 }
-
-
+define('DATA_JSON', __DIR__ . '/../lib/data/users.json');
+define('BASE_URL', rtrim(WEB_ROOT, '/'));
 
 
