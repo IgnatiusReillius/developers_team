ee8f36a9ccd0c6011e5bff0e8d380900474d1870
Cristhian-Valenci <cristhianvalenci22@gmail.com>
2025-10-28 14:02:27 +0100

    cambio en el archivo de constantes
    Elimino 2 constantes que no se usaban

--- config/constants.php
+++ config/constants.php
@@ -1,13 +1,7 @@
 <?php
 
-// Ruta absoluta a la raíz del proyecto
-if (!defined('ROOT_PATH')) {
-    define('ROOT_PATH', __DIR__ . '/../'); 
-}
 
-if (!defined('VIEWS_PATH')) {
-    define('VIEWS_PATH', __DIR__ . '/../app/views/');
-}
+
 define('DATA_JSON', __DIR__ . '/../lib/data/users.json');
 define('BASE_URL', rtrim(WEB_ROOT, '/'));
 
