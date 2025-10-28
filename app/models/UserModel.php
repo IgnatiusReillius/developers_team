7dffee09dc1ce72c90a54a0b795ea0797bbc2c94
mikelji97 <mikel.falcesino@gmail.com>
2025-10-28 01:21:55 +0100

    Añadir logica de creacion de usuarios en UserModel

---
+++ app/models/UserModel.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+class UserModel extends Model
+{
+
+    private $data;
+
+    function __construct()
+    {
+        $json = file_get_contents(DATA_JSON);  //he hecho una constante llamada DATA_JSON para facilitar trabajo, en ruta al arhivo json donde guardo los usuarios, utilizado una funcion para obtener su contenido guardandolo en la variable $json.
+        $this->data =  json_decode($json, true); //  aqui lo que se hace es que el contenido cogido en JSON lo pasamos a un array asociativo para poder tratar los datos.
+    }
+    function verifyByEmail(string $email): bool
+    {
+        $users =  $this->getAllUsers();
+        foreach ($users as $user) {
+            if (isset($user['email']) && $user['email'] === $email) {
+                return true;
+            }
+        }
+        return false;
+    }
+    function createUsers(string $email, string $password, string $name)
+    {
+        if ($this->verifyByEmail($email)) {
+            return false;
+        }
+            $newUser =  [
+                'id' => uniqid(),
+                'name' => $name,
+                'email' => $email,
+                'password' => password_hash($password, PASSWORD_DEFAULT)
+            ];
+            $this->data['users'][] = $newUser;
+            $this->saveUsers();
+        return true;
+    }
+    function saveUsers() :bool
+    {
+        $json = json_encode($this->data, JSON_PRETTY_PRINT);
+        return file_put_contents(DATA_JSON, $json) !== false;
+    }
